2f0e71152fee7e574540d1ce7fbe9a696476f295
PC - Enzo Ferey <shoon96@gmail.com>
2018-06-14 20:30:48 +0200

    Base Slate setup and environment.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Slate from "./Slate";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <div className="limits">
+      <Slate />
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/Slate.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Editor } from "slate-react";
+import { Value } from "slate";
+
+const initialValue = Value.fromJSON({
+	document: {
+		nodes: [
+			{
+				object: "block",
+				type: "paragraph",
+				nodes: [
+					{
+						object: "text",
+						leaves: [
+							{
+								text: "A line of text in a paragraph."
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+});
+
+// Define our app...
+class Slate extends React.Component {
+	// Set the initial value when the app is first constructed.
+	state = {
+		value: initialValue
+	};
+
+	// On change, update the app's React state with the new editor value.
+	onChange = ({ value }) => {
+		this.setState({ value });
+	};
+
+	// Render the editor.
+	render() {
+		return <Editor value={this.state.value} onChange={this.onChange} />;
+	}
+}
+
+export default Slate;
